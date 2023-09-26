fb528154da217121faadb290ea1bf005124a3837
秦豪杰 <1485642186@qq.com>
2023-09-26 21:11:29 +0800

    feat: ✨ 新增登录接口

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get ,Param,Query} from '@nestjs/common';
+import { Controller, Get, Post, Param, Query, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,13 +9,45 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
- 
-  @Get('/user/:id')
-  getUserById(@Param() param): string {
-    return 'user id: '+ param.id;
+
+  // @Get('/user/:id')
+  // getUserById(@Param() param): string {
+  //   return 'user id: ' + param.id;
+  // }
+  @Get('/user/list')
+  getUserList(@Param() param): string[] {
+    return ['小田', '小慧', '亚楠'];
   }
   @Get('/user')
-  getUserByQuery(@Query() query): string {
-    return  query;
+  getUserByQuery(@Query() query): any {
+    return query;
+    return {
+      name: '小田很甜',
+      age: 18,
+    };
+  }
+  @Post('/user')
+  getUserByData(@Body() param): any {
+    return param;
+    return {
+      name: '小田很甜',
+      age: 18,
+    };
+  }
+  @Post('/login')
+  login(@Body() param): any {
+    const res = param.userName.includes('admin')
+      ? {
+          msg: '登录成功',
+          code: 200,
+          token: '123f21fds2f1f2sdfsd1ghhg2h',
+        }
+      : {
+          msg: '用户不存在',
+          code: 500,
+          data: null,
+        };
+
+    return res;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); //node设置跨域
   await app.listen(3000);
 }
 bootstrap();
